c5c0a9fcfddf85beabf95acb823376c060912725
Diego Prado <kaoxdc@gmail.com>
2023-08-04 14:47:49 -0500

    primer commit componentes, JSX, Fragments

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+// root.render(<h1>Hola !!!!!</h1>)
+function Saludar() {
+
+    const estado = true
+    const persona = {
+        nombre: 'Diego',
+        apellido: 'Prado',
+    }
+    if (estado) {
+        return <h2> {persona.nombre}  Esta Vivo 😄</h2>
+    } else {
+        return <h2> {persona.nombre} Ya no vive 🥶</h2>
+    }
+    // alternativa a un IF ELSE anterior 
+    return <h2> {estado ? 'Estoy Vivo :D 😄' : 'Ya no vivo :C 🥶'} </h2>
+
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    // Fragment <></> = es un componente para agrupar elementos sin introducir elementos extra a nivel de DOM
+    <>
+        <Saludar />
+    </>
+)
