bdbeac668dd0963c0c73826554c81493312a096f
Diego Prado <kaoxdc@gmail.com>
2023-08-08 20:57:22 -0500

    Manejador de Eventos fixed

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Tarjeta } from './Tarjeta';
 import { Saludar } from './Saludar';
 import Producto from './Producto';
 import { Boton } from './Boton';
+import { Posts } from './Posts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(<h1>Hola !!!!!</h1>)
@@ -19,21 +20,26 @@ root.render(
     <>
         <Boton texto='Enviar' />
         <Tarjeta estado={true} />
-
+        <p> Input Text con evento onClick </p>
         <input onClick={function () {
             alert('Selecciono el input text')
-            }
-        } />
-        <input onChange={function (e) {
+        }
+    } />
+        <p> Input Text con evento onChange con parametros </p>
+        <input id='HOLAAAA' onChange={function (e) {
             // console.log('escribiendo en el input')
-            console.log(e)
-            }
-        } />
+            console.log(e.target.id)
+        }
+    } />
+        <p> Input Text con evento onChange con una funcion de flecha </p>
         <input onChange={handleChange} />
+
         <form onSubmit={() => console.log('enviando datos desde el formulario')}>
             <h1>Agregar Producto</h1>
             <input type="submit" value='sent'></input>
             <button>Enviar</button>
         </form>
+        
+        {/* <Posts /> */}
     </>
 )
