2f75a30f6c1c68167839476cf1425caf19fcb302
Diego Prado <kaoxdc@gmail.com>
2023-08-08 19:47:00 -0500

    Manejador de Eventos

--- src/index.js
+++ src/index.js
@@ -9,22 +9,31 @@ import { Boton } from './Boton';
 const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(<h1>Hola !!!!!</h1>)
 
+const handleChange = (e) => {
+    // console.log('escribiendo en el input')
+    console.log(e.target.value)
+    }
 
 root.render(
     // Fragment <></> = es un componente para agrupar elementos sin introducir elementos extra a nivel de DOM
-    <>  
-        <Boton texto = 'Enviar' />
-        <Tarjeta estado = {true} />
+    <>
+        <Boton texto='Enviar' />
+        <Tarjeta estado={true} />
 
-
-        <Saludar />
-        <Producto 
-            nombre="Agua" 
-            precio={2500} 
-            disponible={true}
-            categorias={[1,2,3]}
-            marca={ {nombre:"marca 1"} } 
-        />
-        
+        <input onClick={function () {
+            alert('Selecciono el input text')
+            }
+        } />
+        <input onChange={function (e) {
+            // console.log('escribiendo en el input')
+            console.log(e)
+            }
+        } />
+        <input onChange={handleChange} />
+        <form onSubmit={() => console.log('enviando datos desde el formulario')}>
+            <h1>Agregar Producto</h1>
+            <input type="submit" value='sent'></input>
+            <button>Enviar</button>
+        </form>
     </>
 )
