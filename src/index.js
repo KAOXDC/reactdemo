e133f7e800383f4b5038bc62fa8c4229cbf152c6
Diego Prado <kaoxdc@gmail.com>
2023-08-11 14:27:37 -0500

    useState

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 
 import { Tarjeta } from './Tarjeta';
@@ -10,36 +10,37 @@ import { Posts } from './Posts';
 const root = ReactDOM.createRoot(document.getElementById('root'))
 // root.render(<h1>Hola !!!!!</h1>)
 
-const handleChange = (e) => {
-    // console.log('escribiendo en el input')
-    console.log(e.target.value)
-    }
+function Contador() {
+    // let contador = 0
+
+    const [contador, setContador] = useState(0)
+    const [mensaje, setMensaje] = useState('')
+
+    return (
+        <div>
+            <h1>Contador = {contador} </h1>
+            <button onClick={() => {
+                setContador(contador + 1)
+            }}>Incrementar +1 </button>
+
+            <button onClick={() => {
+                setContador(contador - 1)
+            }}>Decrementar -1 </button>
+            <hr></hr>
+            <input onChange={e => setMensaje( e.target.value )} />
+            <button onClick={ () => {
+                alert(mensaje)
+            } } > Guardar </button>
+        </div>
+
+
+    )
+}
 
 root.render(
     // Fragment <></> = es un componente para agrupar elementos sin introducir elementos extra a nivel de DOM
     <>
-        <Boton texto='Enviar' />
-        <Tarjeta estado={true} />
-        <p> Input Text con evento onClick </p>
-        <input onClick={function () {
-            alert('Selecciono el input text')
-        }
-    } />
-        <p> Input Text con evento onChange con parametros </p>
-        <input id='HOLAAAA' onChange={function (e) {
-            // console.log('escribiendo en el input')
-            console.log(e.target.id)
-        }
-    } />
-        <p> Input Text con evento onChange con una funcion de flecha </p>
-        <input onChange={handleChange} />
-
-        <form onSubmit={() => console.log('enviando datos desde el formulario')}>
-            <h1>Agregar Producto</h1>
-            <input type="submit" value='sent'></input>
-            <button>Enviar</button>
-        </form>
-        
         {/* <Posts /> */}
+        <Contador />
     </>
 )
